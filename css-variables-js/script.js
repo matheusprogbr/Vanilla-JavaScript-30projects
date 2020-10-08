2ad68bc036245f929d19b387cf473ae0c5babf9e
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-07 22:20:48 -0300

    feat: Add blur, spacing and color features

--- css-variables-js/script.js
+++ css-variables-js/script.js
@@ -1,7 +1,10 @@
-const controls = document.querySelector('.controls');
+const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate(event) {
-  console.log(event.target.value);
-};
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
 
 controls.addEventListener('change', handleUpdate);
